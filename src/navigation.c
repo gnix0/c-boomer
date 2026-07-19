2f5b603ca3f198a61d245295dec69b303950dd0d
gnix0 <dev.gustavoa@gmail.com>
2026-07-19 20:22:23 -0300

    chore: fix formatting

--- src/navigation.c
+++ src/navigation.c
@@ -16,11 +16,13 @@ void update(Camera *camera, Config config, double dt, Mouse mouse,
 
         Vec2f p0 = (vec_div_scalar(
             vec_sub(camera->scale_pivot, scaled_window_size), camera->scale));
+
         camera->scale =
             fmax(camera->scale + camera->delta_scale * dt, config.min_scale);
 
         Vec2f p1 = (vec_div_scalar(
             vec_sub(camera->scale_pivot, scaled_window_size), camera->scale));
+
         camera->position = vec_add(camera->position, vec_sub(p0, p1));
 
         camera->delta_scale -= camera->delta_scale * dt * config.scale_friction;
@@ -29,6 +31,7 @@ void update(Camera *camera, Config config, double dt, Mouse mouse,
     if (!mouse.drag && (vec_length(camera->velocity) > VELOCITY_THRESHOLD)) {
         camera->position =
             vec_add(camera->position, vec_scale(camera->velocity, dt));
+        
         camera->velocity =
             vec_sub(camera->velocity, vec_scale(vec_scale(camera->velocity, dt),
                                                 config.drag_friction));
